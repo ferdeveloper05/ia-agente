db120db2d5e1f6b358b4665e447a47b774152e47
ferdeveloper05 <fer.developer05@gmail.com>
2026-01-20 10:34:55 -0400

    Subiendo los nuevos cambios

--- app/langchain_agent.py
+++ app/langchain_agent.py
@@ -2,17 +2,20 @@ from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import PromptTemplate, ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage, AIMessage
 
+# Llamada al servidor del modelo (OllamaLLM)
 llm = OllamaLLM(
     model="gpt-oss:20b", 
-    base_url="http://10.50.95.144:11440",
+    base_url="http://10.90.20.12:11440",
     temperature=0.7
 )
 
+# Plantilla base para proporcionar informacion al modelo
 prompt = PromptTemplate(
     input_variables=["question"], 
     template="Eres un experto en {question} responde con claridad",
 )
 
+# Enviar al chat y guardar en el historial la conversacion 
 chat_prompt = ChatPromptTemplate([
     ("system", "Eres un asistente util responde con claridad"),
     MessagesPlaceholder(variable_name="historial"),
@@ -21,12 +24,12 @@ chat_prompt = ChatPromptTemplate([
 
 historial = []
 
+# Establece la cadena de conversacion
 chain = chat_prompt | llm
 
 def agente(question: str) -> str:
     global historial
-    #if historial is None: 
-        #historial = [] 
+    
     response = chain.invoke({"question": question, "historial":historial}) 
     
     historial.append(HumanMessage(content=question))
--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI           
+from fastapi import FastAPI,HTTPException       
 from pydantic import BaseModel
 from langchain_agent import agente
 import requests
@@ -11,13 +11,13 @@ def say_hello():
     return "Hola Mundo"
 
 class Query(BaseModel):
-    question: str
+    question: str 
 
 
 @app.get("/check-ollama")
 def check_ollama():
     try:
-        response = requests.get("http://10.50.95.144:11440/api/tags")
+        response = requests.get("http://10.90.20.12:11440/api/tags")
         return {"status": "Ollama activo", "modelos": response.json()}
     except Exception as e:
         return {"status": "Error de conexión", "detalle": str(e)}
@@ -27,7 +27,7 @@ def ask(query: Query):
     answer = agente(query.question)
     return {"response": answer}
 
-
+# Funcion encargada de obtener la pregunta del usuario
 @app.post("/pregunta", tags=['Pruebas'])
 def pregunta(query: Query):
     respuesta = agente(query.question)
